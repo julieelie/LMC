46eda61cd5281ec033928a5b8ee0a3f4fdc13a61
Julie E Elie <julieelie@users.noreply.github.com>
2018-11-19 11:29:54 -0800

    Update wrapper_script_extract_logger.m

---
+++ wrapper_script_extract_logger.m
@@ -0,0 +1,24 @@
+%% Extract the audio logger data of the days we are interested in
+script_extract_loggers('06272018',{'65430' '71300' '71354' '71335' '71332'} , [5 6 3 7 9])
+script_extract_loggers('06282018',{'65430' '71300' '71354' '71213' '71137'} , [5 6 3 7 9])
+script_extract_loggers('07022018',{'65430' '65430' '71300' '71300' '59899'} , [5 7 9 6 3])
+script_extract_loggers('07032018',{'65430' '65430' '71300' '71300' '71354'} , [5 7 9 6 3])
+script_extract_loggers('07062018',{'65430' '71300' '60014'} , [5 6 3])
+script_extract_loggers('07102018',{'65430' '71300' '59899' '71132' '71213'} , [5 6 10 9 7])
+script_extract_loggers('07112018',{'65430' '71300' '59899' '71132' '71335'} , [5 6 10 9 7])
+script_extract_loggers('07122018',{'65430' '71300' '71173' '71137' '71213'} , [5 6 10 9 7])
+script_extract_loggers('07242018',{'65430' '71300' '59899' '71132' '71335'} , [5 6 10 9 7])
+script_extract_loggers('07252018',{'65430' '71300' '60014' '71132' '71213'} , [5 6 10 9 7])
+script_extract_loggers('07262018',{'65430' '71300' '71173' '71137' '71335'} , [5 6 10 9 7])
+
+%% Allign TTL pulses for all these days
+Days = {'06272018', '06282018', '07022018', '07032018', '07062018', '07102018', '07112018', '07122018', '07242018', '07252018', '07262018'};
+Server_logger_path = 'Z:\users\Julie E\GiMo_65430_71300\Loggers';
+Server_audio_path = 'Z:\users\Julie E\GiMo_65430_71300\Audio';
+for dd=1:length(Days)
+    Loggers_dir = fullfile(Server_logger_path, Days{dd});
+    Audio_dir = fullfile(Server_audio_path, Days{dd});
+ExpStartTime = '1059';
+
+align_soundmexAudio_2_logger(Audio_dir, Loggers_dir, ExpStartTime,'Method','rise');
+end
