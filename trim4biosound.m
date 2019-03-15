7f6527c7cbadbfaf310fc40769415a250e570629
Julie Elie <julie.elie@gmail.com>
2019-03-14 17:59:21 -0700

    center,bound and filter the piezo sound before biosound

--- trim4biosound.m
+++ trim4biosound.m
@@ -2,7 +2,8 @@ function []=trim4biosound(Loggers_dir, Date, ExpStartTime, MergeThresh, Manual)
 Loggers_dir = '/Users/elie/Documents/ManipBats/LMC/190110_59882_11689_HoHa/20190130_TestCalls';
 Date = num2str(190130);
 ExpStartTime = num2str(1007);
-MergeThresh=200;
+MergeThresh=200; %ms
+BandPassFiltPiezo = [100 10000]; % bandpass filter of the piezo
 
 %% Load the data
 load(fullfile(Loggers_dir, sprintf('%s_%s_VocExtractData.mat', Date, ExpStartTime)), 'Piezo_wave', 'Piezo_FS',  'Raw_wave','FS','BandPassFilter', 'AudioLogs','VocFilename');
@@ -38,11 +39,17 @@ for ss=1:Nsound
         fprintf(1,'    ->Mic\n')
         Y = Raw_wave{ss}(IndVocStartRaw_merged{ss}{VocBatInd}(cc):IndVocStopRaw_merged{ss}{VocBatInd}(cc));
         Filename_local = fullfile(OutputDirMic, sprintf('%s_mic_%d.wav',File,cc));
+        Y=Y-mean(Y);% center the sound
         audiowrite(Filename_local, Y, FS);
         fprintf(1,'    ->Piezo\n')
         Y = Piezo_wave.(LoggerNames{VocBatInd}){ss}(IndVocStartPiezo_merged{ss}{VocBatInd}(cc):IndVocStopPiezo_merged{ss}{VocBatInd}(cc));
+        [z,p,k] = butter(6,BandPassFiltPiezo/(Piezo_FS.(LoggerNames{VocBatInd})(ss)/2),'bandpass');
+        sos_low = zp2sos(z,p,k);
+        Y = Y-mean(Y); % center the output of the piezo
+        Y = Y/max(abs(Y)); % scale it between -1 and 1
+        YLow = (filtfilt(sos_low,1,Y)); % low-pass filter the voltage trace
         Filename_local = fullfile(OutputDirPiezo, sprintf('%s_piezo_%d_%s.wav',File,cc,LoggerNames{VocBatInd}));
-        audiowrite(Filename_local, Y, round(Piezo_FS.(LoggerNames{VocBatInd})(ss)));
+        audiowrite(Filename_local, YLow/max(abs(YLow)), round(Piezo_FS.(LoggerNames{VocBatInd})(ss)));
     end
         
 end
