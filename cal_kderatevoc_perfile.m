ef55a7393931683e1b48e9f97dd29325762ffc27
Julie Elie <julie.elie@gmail.com>
2019-12-04 17:02:03 -0800

    adding a calculation of KDE alligned to vocalization offset

--- cal_kderatevoc_perfile.m
+++ cal_kderatevoc_perfile.m
@@ -45,86 +45,106 @@ IndBa = find(contains(Data.What, 'VocBa'));
 
 %% KDE time-varying rate alligned to vocalization production onset/offset
 if ~isempty(IndVocPD) && ~isempty(IndVocPDO) && ~isempty(IndVocPDF)
-    [KDE.SelfVocAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocPD),Data.Duration(IndVocPD),Delay,Bin_ms);
+    [KDE_onset.SelfVocAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocPD),Data.Duration(IndVocPD),Delay,Bin_ms);
+    [KDE_offset.SelfVocAll] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndVocPD),Data.Duration(IndVocPD),Delay,Bin_ms);
+    
     IndTrPD = intersect(IndTr, IndVocPD);
     if length(IndTrPD)>MinNumCall
-        [KDE.SelfTrAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrPD),Data.Duration(IndTrPD),Delay,Bin_ms);
+        [KDE_onset.SelfTrAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrPD),Data.Duration(IndTrPD),Delay,Bin_ms);
+        [KDE_offset.SelfTrAll] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndTrPD),Data.Duration(IndTrPD),Delay,Bin_ms);
+    
     end
     IndBaPD = intersect(IndBa, IndVocPD);
     if length(IndBaPD)>MicNumCall
-        [KDE.SelfBaAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaPD),Data.Duration(IndBaPD),Delay,Bin_ms);
+        [KDE_onset.SelfBaAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaPD),Data.Duration(IndBaPD),Delay,Bin_ms);
+        [KDE_offset.SelfBaAll] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndBaPD),Data.Duration(IndBaPD),Delay,Bin_ms);
     end
 end
 
 %% KDE time-varying rate alligned to vocalization perception onset/offset
 if ~isempty(IndVocHD) && ~isempty(IndVocHDO) && ~isempty(IndVocHDF)
-    [KDE.OthersVocAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocHD),Data.Duration(IndVocHD),Delay,Bin_ms);
+    [KDE_onset.OthersVocAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocHD),Data.Duration(IndVocHD),Delay,Bin_ms);
+    [KDE_offset.OthersVocAll] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndVocHD),Data.Duration(IndVocHD),Delay,Bin_ms);
     IndTrHD = intersect(IndTr, IndVocHD);
     if length(IndTrHD)>MinNumCall
-        [KDE.OthersTrAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrHD),Data.Duration(IndTrHD),Delay,Bin_ms);
+        [KDE_onset.OthersTrAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrHD),Data.Duration(IndTrHD),Delay,Bin_ms);
+        [KDE_offset.OthersTrAll] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndTrHD),Data.Duration(IndTrHD),Delay,Bin_ms);
     end
     IndBaHD = intersect(IndBa, IndVocHD);
     if length(IndBaHD)>MinNumCall
-        [KDE.OthersBaAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaHD),Data.Duration(IndBaHD),Delay,Bin_ms);
+        [KDE_onset.OthersBaAll] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaHD),Data.Duration(IndBaHD),Delay,Bin_ms);
+        [KDE_offset.OthersBaAll] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndBaHD),Data.Duration(IndBaHD),Delay,Bin_ms);
     end
 end
 
 %% KDE time-varying rate alligned to vocalization production onset/offset during Operant conditioning
 if ~isempty(IndVocPDO)
-    [KDE.SelfVocOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocPDO),Data.Duration(IndVocPDO),Delay,Bin_ms);
+    [KDE_onset.SelfVocOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocPDO),Data.Duration(IndVocPDO),Delay,Bin_ms);
+    [KDE_offset.SelfVocOp] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndVocPDO),Data.Duration(IndVocPDO),Delay,Bin_ms);
     IndTrPDO = intersect(IndTr, IndVocPDO);
     if length(IndTrPDO)>MinNumCall
-        [KDE.SelfTrOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrPDO),Data.Duration(IndTrPDO),Delay,Bin_ms);
+        [KDE_onset.SelfTrOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrPDO),Data.Duration(IndTrPDO),Delay,Bin_ms);
+        [KDE_offset.SelfTrOp] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndTrPDO),Data.Duration(IndTrPDO),Delay,Bin_ms);
     end
     IndBaPDO = intersect(IndBa, IndVocPDO);
     if length(IndBaPDO)>MinNumCall
-        [KDE.SelfBaOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaPDO),Data.Duration(IndBaPDO),Delay,Bin_ms);
+        [KDE_onset.SelfBaOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaPDO),Data.Duration(IndBaPDO),Delay,Bin_ms);
+        [KDE_offset.SelfBaOp] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndBaPDO),Data.Duration(IndBaPDO),Delay,Bin_ms);
     end
 end
 
 %% KDE time-varying rate alligned to vocalization perception onset/offset during operant conditioning
 if ~isempty(IndVocHDO)
-    [KDE.OthersVocOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocHDO),Data.Duration(IndVocHDO),Delay,Bin_ms);
+    [KDE_onset.OthersVocOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocHDO),Data.Duration(IndVocHDO),Delay,Bin_ms);
+    [KDE_offset.OthersVocOp] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndVocHDO),Data.Duration(IndVocHDO),Delay,Bin_ms);
     IndTrHDO = intersect(IndTr, IndVocHDO);
     if length(IndTrHDO)>MinNumCall
-        [KDE.OthersTrOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrHDO),Data.Duration(IndTrHDO),Delay,Bin_ms);
+        [KDE_onset.OthersTrOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrHDO),Data.Duration(IndTrHDO),Delay,Bin_ms);
+        [KDE_offset.OthersTrOp] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndTrHDO),Data.Duration(IndTrHDO),Delay,Bin_ms);
     end
     IndBaHDO = intersect(IndBa, IndVocHDO);
     if length(IndBaPDO)>MinNumCall
-        [KDE.OthersBaOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaHDO),Data.Duration(IndBaHDO),Delay,Bin_ms);
+        [KDE_onset.OthersBaOp] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaHDO),Data.Duration(IndBaHDO),Delay,Bin_ms);
+        [KDE_offset.OthersBaOp] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndBaHDO),Data.Duration(IndBaHDO),Delay,Bin_ms);
     end
 end
 
 %% KDE time-varying rate alligned to vocalization production onset/offset during Free session
 if ~isempty(IndVocPDF)
-    [KDE.SelfVocFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocPDF),Data.Duration(IndVocPDF),Delay,Bin_ms);
+    [KDE_onset.SelfVocFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocPDF),Data.Duration(IndVocPDF),Delay,Bin_ms);
+    [KDE_offset.SelfVocFr] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndVocPDF),Data.Duration(IndVocPDF),Delay,Bin_ms);
     IndTrPDF = intersect(IndTr, IndVocPDF);
     if length(IndTrPDF)>MinNumCall
-        [KDE.SelfTrFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrPDF),Data.Duration(IndTrPDF),Delay,Bin_ms);
+        [KDE_onset.SelfTrFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrPDF),Data.Duration(IndTrPDF),Delay,Bin_ms);
+        [KDE_offset.SelfTrFr] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndTrPDF),Data.Duration(IndTrPDF),Delay,Bin_ms);
     end
     IndBaPDF = intersect(IndBa, IndVocPDF);
     if length(IndBaPDF)>MinNumCall
-        [KDE.SelfBaFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaPDF),Data.Duration(IndBaPDF),Delay,Bin_ms);
+        [KDE_onset.SelfBaFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaPDF),Data.Duration(IndBaPDF),Delay,Bin_ms);
+        [KDE_offset.SelfBaFr] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndBaPDF),Data.Duration(IndBaPDF),Delay,Bin_ms);
     end
 end
 
 %% KDE time-varying rate alligned to vocalization perception onset/offset during Free session
 if ~isempty(IndVocHDF)
-    [KDE.OthersVocFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocHDF),Data.Duration(IndVocHDF),Delay,Bin_ms);
+    [KDE_onset.OthersVocFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndVocHDF),Data.Duration(IndVocHDF),Delay,Bin_ms);
+    [KDE_offset.OthersVocFr] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndVocHDF),Data.Duration(IndVocHDF),Delay,Bin_ms);
     IndTrHDF = intersect(IndTr, IndVocHDF);
     if length(IndTrHDF)>MinNumCall
-        [KDE.OthersTrFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrHDF),Data.Duration(IndTrHDF),Delay,Bin_ms);
+        [KDE_onset.OthersTrFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndTrHDF),Data.Duration(IndTrHDF),Delay,Bin_ms);
+        [KDE_offset.OthersTrFr] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndTrHDF),Data.Duration(IndTrHDF),Delay,Bin_ms);
     end
     IndBaHDF = intersect(IndBa, IndVocHDF);
     if length(IndBaPDO)>MinNumCall
-        [KDE.OthersBaFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaHDF),Data.Duration(IndBaHDF),Delay,Bin_ms);
+        [KDE_onset.OthersBaFr] = kderate_onset(Data.SpikesArrivalTimes_Behav(IndBaHDF),Data.Duration(IndBaHDF),Delay,Bin_ms);
+        [KDE_offset.OthersBaFr] = kderate_offset(Data.SpikesArrivalTimes_Behav(IndBaHDF),Data.Duration(IndBaHDF),Delay,Bin_ms);
     end
 end
 
 
 
 % save data
-save(FullDataSetFile, 'KDE', '-append')
+save(FullDataSetFile, 'KDE_onset','KDE_offset', '-append')
 
 
 %% INTERNAL FUNCTION
@@ -145,12 +165,14 @@ save(FullDataSetFile, 'KDE', '-append')
 
     function [OUT] = kderate_offset(SpikesArrivalTimes,Duration,Delay, Bin_ms)
         Response_samprate = 1/Bin_ms;% Sampling rate of the KDE in kHz
-        % Find the number of events for each time window
-        t=-Delay(1): Bin_ms : round((max(Duration) + Delay(2))/Bin_ms)*Bin_ms;
+        % Find the number of events for each time window and center spikes
+        % to vocalization offset
+        t = -round((max(Duration) + Delay(2))/Bin_ms)*Bin_ms : Bin_ms : Delay(1);
         Weight = zeros(1,length(t));
         for vv=1:length(Duration)
-            t_local = -Delay(1): Bin_ms : (Duration(vv) + Delay(2));
-            Weight(1:length(t_local))=Weight(1:length(t_local))+1;
+            t_local = -(Duration(vv) + Delay(2)) : Bin_ms : Delay(1);
+            Weight((length(t)-length(t_local)+1):length(t))=Weight((length(t)-length(t_local)+1):length(t))+1;
+            SpikesArrivalTimes{vv} = SpikesArrivalTimes{vv}-Duration(vv);
         end
         % calculated KDE
         AllSpikes_local = cell2mat(SpikesArrivalTimes);
